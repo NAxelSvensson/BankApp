069eb6d8df11e522428a8bbf7352f368fcd94229
Axel <n.axelsvensson@gmail.com>
2023-02-20 11:25:01 +0100

    Can't login with an account that doesn't exist

--- BankAppClass.py
+++ BankAppClass.py
@@ -40,23 +40,9 @@ class Bank:
         self.customers = []
         self.current_customer = None
     
-    #Funktion för att hämta alla kunder
-    def get_customers(self):
-        for customer in self.customers:
-            print(customer.name, customer.password)
-        return self.customers
-    
     #Funktion för att lägga till kund
     def add_customer(self, customer):
         self.customers.append(customer)
-    
-    #Funktion för att hämta en specifik kund
-    def get_customer(self, name):
-        for customer in self.customers:
-            if customer.name == name:
-                print(f"Name: {customer.name}, Password: {customer.password}")
-                return customer
-        return None
     
     #Funktion för att ändra en kunds lösenord
     def change_customer_password(self, name, new_password):
@@ -66,22 +52,12 @@ class Bank:
                 return True
         return False
     
-    #Funktion för att ta bort en kund
-    def remove_customer(self, name):
-        for customer in self.customers:
-            if customer.name == name:
-                self.customers.remove(customer)
-                return True
-        return False
-    
     #Funktion för att logga in
     def login(self, username, password):
         for customer in self.customers:
             if customer.name == username and customer.password == password:
                 self.current_customer = customer
                 return True
-        #Om man försöker logga in med en ny kund och skriver in fel användarnamn eller lösenord 
-        self.current_customer = None                     #så loggas gamla kunden ut automatiskt
         return False
     
     #Funktion för att logga ut
@@ -102,12 +78,10 @@ class Bank:
     
     #Funktion för att ta bort ett konto för den inloggade kunden
     def remove_account(self, number):
-        #x = 0
         for account in self.current_customer.get_accounts():
             if account.get_numbers() == number:
                 self.current_customer.get_accounts().remove(account)
                 return True
-            #x +=1
         return False
     
     #Funktion för att få ett specifikt konto från den inloggade kunden
@@ -125,9 +99,7 @@ class Bank:
         if self.current_customer:
             for account in self.current_customer.get_accounts():
                 if account.get_numbers() == number:
-                    print(f"Kontonummer: {account.get_numbers()}, Pengar: {account.get_money()}kr")
                     account.deposit(money)
-                    print(f"Kontonummer: {account.get_numbers()}, Pengar: {account.get_money()}kr")
                     return True
             return False
     
@@ -137,17 +109,6 @@ class Bank:
             for account in self.current_customer.get_accounts():
                 if account.get_numbers() == number:
                     if account.money >= money: 
-                        print(f"Kontonummer: {account.get_numbers()}, Pengar: {account.get_money()}kr")
                         account.withdraw(money)
-                        print(f"Kontonummer: {account.get_numbers()}, Pengar: {account.get_money()}kr")
                         return True
-                    else:
-                        print("You don't have enough money")
             return False
-    
-    #Funktion för att kunna se om någon är inloggad och isådanna fall vem som är det
-    def print_current_customer(self):
-        if self.current_customer != None:
-            print(f"Logged in user is {self.current_customer.name} with password {self.current_customer.password}")
-        else:
-            print("No one is logged in")
